7517699d180d3416822ffe11186fc7e3993ac38c
marianoruiza <marianoruiz145@gmail.com>
2024-09-17 01:05:37 -0300

    DB CONECT

--- dataBase/db.js
+++ dataBase/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const URI = process.env.MONGO_URI
-const DB  = process.env.MONGO_DB
+//const DB  = process.env.MONGO_DB
 
 const conectDB = async  () => {
     try {
-        await mongoose.connect(`${URI}/${DB}`,)
-        console.log("concectado a la base de datos") 
+        await mongoose.connect(`${URI}`)
+        console.log("conectado a la base de datos") 
     } catch (error) {
         console.log(error)
     }
